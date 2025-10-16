5d4aaec66823e9394e31c224156821198bcb69af
Anthony Irwin <anthony@anthonyirwin.com>
2025-10-16 12:41:25 +1000

    add postcss and purgecss to remove unused css from the final build of the website

---
+++ postcss.config.js
@@ -0,0 +1,37 @@
+const { PurgeCSS } = require('purgecss');
+
+const purgeCSSPlugin = () => {
+  return {
+    postcssPlugin: 'postcss-purgecss-modern',
+    Once: async (root, { result }) => {
+      if (process.env.HUGO_ENVIRONMENT !== 'production') return;
+
+      const purgeCSSResult = await new PurgeCSS().purge({
+        content: [
+          './layouts/**/*.html',
+          './content/**/*.md',
+          './assets/js/**/*.js',
+          './hugo_stats.json',
+        ],
+        css: [{ raw: root.toString() }],
+        defaultExtractor: content =>
+          content.match(/[\w-/:.]+(?<!:)/g) || [],
+      });
+
+      if (purgeCSSResult.length > 0 && purgeCSSResult[0].css) {
+        // Replace the entire CSS with the purged version
+        root.removeAll();
+        root.append(require('postcss').parse(purgeCSSResult[0].css));
+      }
+    },
+  };
+};
+
+purgeCSSPlugin.postcss = true;
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    purgeCSSPlugin(),
+  ],
+};
